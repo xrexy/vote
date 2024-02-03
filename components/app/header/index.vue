2adac7cf608ec27fcbb8d1add622af03501911c2
xrexy <rexydoesimportantthings@gmail.com>
2024-02-03 22:11:28 +0200

    feat: header logo not functional

--- components/app/header/index.vue
+++ components/app/header/index.vue
@@ -1,7 +1,10 @@
 <template>
   <header>
     <div class="relative border-b border-border flex flex-row justify-between items-center rounded-md px-8 py-4">
-      <AppLogo />
+      <NuxtLink to="/">
+        <AppLogo />
+      </NuxtLink>
+
       <AppHeaderSearch class="absolute left-[50%] -translate-x-[50%]" />
       <div class="flex gap-4">
         <AppHeaderActions />
