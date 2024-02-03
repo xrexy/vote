2a7b30d958e4c0f6d324bd44d0fb3e8b053d8711
xrexy <rexydoesimportantthings@gmail.com>
2024-02-03 22:08:42 +0200

    fix(ui): add gap next to seru

--- components/app/header/index.vue
+++ components/app/header/index.vue
@@ -3,7 +3,7 @@
     <div class="relative border-b border-border flex flex-row justify-between items-center rounded-md px-8 py-4">
       <AppLogo />
       <AppHeaderSearch class="absolute left-[50%] -translate-x-[50%]" />
-      <div class="flex ">
+      <div class="flex gap-4">
         <AppHeaderActions />
         <UserAvatar />
       </div>
