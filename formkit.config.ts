96bd2ca2e5441c31ff6d20bc30e6052a077e7e39
xrexy <rexydoesimportantthings@gmail.com>
2024-02-08 19:21:44 +0200

    feat: add formkit

---
+++ formkit.config.ts
@@ -0,0 +1,8 @@
+import { defineFormKitConfig } from '@formkit/vue'
+import { rootClasses } from './formkit.theme'
+ 
+export default defineFormKitConfig({
+  config: {
+    rootClasses
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const animate = require("tailwindcss-animate")
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
+  content: ['formkit.theme.ts'],
   
   theme: {
     container: {
