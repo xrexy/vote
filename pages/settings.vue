5eacba1d96f1563adaf9ee757c2b0e0fbe8747ff
xrexy <rexydoesimportantthings@gmail.com>
2024-02-12 23:42:55 +0200

    fix: route and currentTab getting out of sync

--- pages/settings.vue
+++ pages/settings.vue
@@ -9,10 +9,13 @@
         v-for="[tab, { icon }] in entries"
         :key="tab"
         variant="ghost"
+        :disabled="currentTab == tab"
         :class="
           cn(
-            'justify-start group text-lg',
-            currentTab == tab && 'text-emerald-400 hover:text-emerald-400'
+            'justify-start group text-lg utline outline-1 outline-transparent hover:text-emerald-300',
+            currentTab == tab && 'text-emerald-600',
+            tab == 'danger-zone' &&
+              'bg-red-600/10 text-red-400 border border-red-600 border-opacity-25 hover:border-opacity-50 hover:bg-red-700/20 hover:text-red-400/90 transition duration-200'
           )
         "
         @click="currentTab = tab"
@@ -113,5 +116,8 @@ function queryToTab(rawParam: LocationQueryValue | LocationQueryValue[]): Tab {
 
 const currentTab = ref(queryToTab(route.query.tab));
 watch(currentTab, updateTabQuery);
+watchEffect(() => {
+  currentTab.value = queryToTab(route.query.tab);
+});
 </script>
 
