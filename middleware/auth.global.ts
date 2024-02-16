40eaa950cbf45ab96c6c099addc1e9258e1a20fb
xrexy <rexydoesimportantthings@gmail.com>
2024-02-16 16:01:02 +0200

    fix: auth middleware using useFetch
    ref: https://github.com/lucia-auth/examples/pull/26
    
    Co-authored-by: Sayykii <Sayykii@users.noreply.github.com>

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,12 +2,9 @@
 const protectedRoutes: (RegExp | string)[] = [new RegExp("/studio/.*")];
 
 export default defineNuxtRouteMiddleware(async (to) => {
-	const user = useUser();
-  // NOTE: has to be useFetch cause $fetch doesn't handle cookies correctly
-	const { data } = await useFetch("/api/auth/me");
-	if (data.value) {
-		user.value = data.value;
-	}
+  const userStore = useUser();
+  const user = await useRequestFetch()("/api/auth/me");
+  userStore.value = user
 
   if (!user) {
     const next = to.path;
