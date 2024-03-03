051da4abb707e1fa5a02a706194d5207d346a111
xrexy <rexydoesimportantthings@gmail.com>
2024-03-03 20:32:04 +0200

    feat: add skeleton to user avatar

--- components/user/avatar/index.vue
+++ components/user/avatar/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="grid place-items-center rounded-full relative w-[48px] h-[48px] p-[2px] border border-border">
-    <Avatar class="border border-border w-full h-full">
-      <AvatarImage
-        :src="`https://crafatar.com/avatars/${user.uuid || 'eb351c77-3f4c-40e2-8f80-270cbb7cce24'
-        }`"
-      />
+    <Skeleton
+      v-if="!imageSource"
+      class="w-full h-full aspect-square rounded-full"
+    />
+    <Avatar
+      v-else
+      class="border border-border w-full h-full"
+    >
+      <AvatarImage :src="imageSource" />
     </Avatar>
     <div
       v-if="!user.uuid"
@@ -15,5 +19,15 @@
 
 <script setup lang="ts">
 import type { User } from "lucia";
-defineProps<{ user: User }>();
+const $props = defineProps<{ user: User }>();
+
+const imageSource = ref<string | null>(null)
+watchEffect(async () => {
+  const uuid = $props.user.uuid
+  const res = await $fetch<Blob>(`https://crafatar.com/avatars/${uuid || 'eb351c77-3f4c-40e2-8f80-270cbb7cce24'}`, {
+    responseType: 'blob',
+  })
+
+  imageSource.value = URL.createObjectURL(res)
+})
 </script>
